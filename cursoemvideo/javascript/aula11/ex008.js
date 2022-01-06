04c5e740735d829da71447824aaa09e893839d1d
mateusjustino <mateus.justino.07@gmail.com>
2022-01-06 11:38:01 -0300

    up

--- cursoemvideo/javascript/aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 50
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel > 60) {
-    console.log(`Você ultrapassou a velocidade permitida. Multado`)
-}
-console.log(`Dirija sempre com cinto de segurança`)
