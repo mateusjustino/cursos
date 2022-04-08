4b36042856d1e4824fbf49f3f79d61f5c9ea206d
mateusjustino <mateus.justino.07@gmail.com>
2022-04-08 16:46:23 -0300

    up

---
+++ cursoemvideo/python/exercicio/ex107/moeda/__init__.py
@@ -0,0 +1,16 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+
+def aumentar(n, qtde):
+    return n / 100 * (100 + qtde)
+
+
+def diminuir(n, qtde):
+    return n / 100 * (100 - qtde)
+
+
