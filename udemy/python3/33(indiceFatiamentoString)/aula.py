65a3a47b0a20eca95d650a4340223de665fb249a
Mateus Justino <87674336+mateusjustino@users.noreply.github.com>
2022-07-10 08:23:35 -0300

    Add files via upload

---
+++ udemy/python3/33(indiceFatiamentoString)/aula.py
@@ -0,0 +1,16 @@
+
+#positivos   [012345]
+texto =      'Python'
+#negativos  -[654321]
+
+print(texto[2]) #ira printar o indice 2, que é o t
+print(texto[2:6]) #ira printar do caracter 2 até o 6
+print(texto[:6]) #ira printar do inicio até o 6
+print(texto[4:]) #ira printar do 4 até o final
+print(texto[-1]) #ira printar o ultimo caracter
+print(texto[-6:-4]) #ira printar do -6 até o -4
+print(texto[:-1]) #ira printar a palavra toda menos o ultimo caracter
+print(texto[0:5:2]) #ira printar do 0 até o 5 a cada 2 caracter
+print(texto[0::3]) #ira printar do 0 até o final a cada 3 caracter
+
+
