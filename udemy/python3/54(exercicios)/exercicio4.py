2a9e0406d5493d27efa550a83be4829b5d486e89
mateusjustino <mateus.justino.07@gmail.com>
2022-09-16 17:34:55 -0300

    up

--- udemy/python3/54(exercicios)/exercicio4.py
+++ udemy/python3/54(exercicios)/exercicio4.py
@@ -1,11 +1,11 @@
 def fizzbuzz(num):
     if (num % 3) == 0 and (num % 5) == 0:
-        print('fizzbuzz - divisivel por 3 e 5')
+        return f'fizzbuzz - divisivel por 3 e 5'
     elif (num % 3) == 0:
-        print('fizz - divisivel por 3')
+        return f'fizz - divisivel por 3'
     elif (num % 5) == 0:
-        print('buzz - divisivel por 5')    
-    else:
-        print(f'{num} - não divisivel por 3 ou 5')
+        return 'buzz - divisivel por 5'
+    return f'{num} - não divisivel por 3 ou 5'
 
-fizzbuzz(9)
+
+print(fizzbuzz(15))
