636494add3bd9e4406e2e6e7affc0885674323e1
mateusjustino <mateus.justino.07@gmail.com>
2024-01-24 18:57:50 -0300

    up

--- my/mariReact/src/pages/Sobre/Sobre.jsx
+++ my/mariReact/src/pages/Sobre/Sobre.jsx
@@ -14,17 +14,19 @@ const Sobre = () => {
             className="vh-custom d-flex flex-column align-items-center justify-content-center img-back"
             id="img-back-1"
           >
-            <div className="container container-max-width content-opacity p-4 rounded-3">
+            <div className="content-opacity py-3 px-5 rounded-3">
               <h1 className="fw-bold text-center">O que somos?</h1>
-              <p className="fs-6">
-                Oie! Muito prazer! Eu sou a Mariana, no momento sou um Studio de
-                Design independente, focada em projetos de identidade visual
-                destinada para empresas/negócios apaixonados no que fazem, e por
-                aqui trabalhamos com um design autoral, não-convencional
-                (criativo), construindo um universo visual de sua marca,
-                alinhado com a expressão de seu negócio.
-              </p>
             </div>
+          </div>
+          <div className="container container-max-width p-5 rounded-3">
+            <p className="fs-6">
+              Oie! Muito prazer! Eu sou a Mariana, no momento sou um Studio de
+              Design independente, focada em projetos de identidade visual
+              destinada para empresas/negócios apaixonados no que fazem, e por
+              aqui trabalhamos com um design autoral, não-convencional
+              (criativo), construindo um universo visual de sua marca, alinhado
+              com a expressão de seu negócio.
+            </p>
           </div>
         </section>
         <section>
@@ -32,24 +34,74 @@ const Sobre = () => {
             className="vh-custom d-flex flex-column align-items-center justify-content-center img-back"
             id="img-back-2"
           >
-            <div className="container container-max-width content-opacity p-4 rounded-3">
+            <div className="content-opacity py-3 px-5 rounded-3">
               <h1 className="fw-bold text-center">Missão</h1>
-              <p className="fs-6">
-                <ul>
-                  <li>
-                    Ser referência na américa latina, e expandir ao mundo, em
-                    ser um studio de design autoral
-                  </li>
-                  <li>Combater réplicas de marcas</li>
-                  <li>Tornar um mundo menos desbotado</li>
-                  <li>Tornar marcas protagonistas em seus próprios nichos</li>
-                  <li>
-                    Construir uma equipe diversificada e apaixonada por criar, e
-                    desfrutar dos seus resultados
-                  </li>
-                </ul>
-              </p>
             </div>
+          </div>
+          <div className="container container-max-width p-5 rounded-3">
+            <p className="fs-6">
+              <ul>
+                <li>
+                  Ser referência na américa latina, e expandir ao mundo, em ser
+                  um studio de design autoral
+                </li>
+                <li>Combater réplicas de marcas</li>
+                <li>Tornar um mundo menos desbotado</li>
+                <li>Tornar marcas protagonistas em seus próprios nichos</li>
+                <li>
+                  Construir uma equipe diversificada e apaixonada por criar, e
+                  desfrutar dos seus resultados
+                </li>
+              </ul>
+            </p>
+          </div>
+        </section>
+        <section>
+          <div
+            className="vh-custom d-flex flex-column align-items-center justify-content-center img-back"
+            id="img-back-1"
+          >
+            <div className="content-opacity py-3 px-5 rounded-3">
+              <h1 className="fw-bold text-center">Valores</h1>
+            </div>
+          </div>
+          <div className="container container-max-width p-5 rounded-3">
+            <p className="fs-6">
+              <ul>
+                <li>
+                  Acredito que toda ideia é valiosa e pode ser experimentada
+                </li>
+                <li>
+                  Todo mundo é extremamente único e diferente, ninguém andará ou
+                  fará o mesmo caminho exatamente como você
+                </li>
+                <li>
+                  Todo erro pra nós é um XP no seu jogo e oportunidade de dar um
+                  upgrade
+                </li>
+              </ul>
+            </p>
+          </div>
+        </section>
+        <section>
+          <div
+            className="vh-custom d-flex flex-column align-items-center justify-content-center img-back"
+            id="img-back-2"
+          >
+            <div className="content-opacity py-3 px-5 rounded-3">
+              <h1 className="fw-bold text-center">
+                Um pouco de minha história
+              </h1>
+            </div>
+          </div>
+          <div className="container container-max-width p-5 rounded-3">
+            <p className="fs-6">
+              Desde que me conheço, sempre tive uma sensibilidade criativa
+              dentro de mim. Desde pequena adorava mexer no computador e jogar
+              vídeo game, e curiosa em como criar coisas novas, desde vídeos,
+              montagens, que na fase adulta consequentemente virou meu trabalho
+              e permaneço amando o que faço!
+            </p>
           </div>
         </section>
       </main>
