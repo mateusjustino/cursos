2d5b7b0d8fc028b9de691585eff7978020b950f1
mateusjustino <mateus.justino.07@gmail.com>
2024-02-26 20:36:00 -0300

    up

--- fda/primeFlix/src/pages/Home/index.jsx
+++ fda/primeFlix/src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
+
 //https://api.themoviedb.org/3/movie/now_playing?api_key=6876ef12b2f4338d57e35ac8d4f4faf8&language=pt-br
 const Home = () => {
-  const [filmes, setFilmes] = useState();
+  const [filmes, setFilmes] = useState([]);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -13,15 +16,30 @@ const Home = () => {
           page: 1,
         },
       });
-      console.log(response.data.results);
+      // console.log(response.data.results.slice(0, 10));
+      // console.log(response.data.results);
+      setFilmes(response.data.results.slice(0, 10));
     }
 
     loadFilmes();
   }, []);
 
   return (
-    <div>
-      <h1>home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 };
